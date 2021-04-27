b9bce9fd6173ba0381bb570f5f5631a0dda8e4d3
Mohammad Saleh Fadaei <ms.fadaei1997@gmail.com>
2021-04-28 01:16:59 +0430

    add example

--- example/pages/index.vue
+++ example/pages/index.vue
@@ -5,7 +5,16 @@
 </template>
 
 <script>
-export default {
+const defaultStyle = 'background: #1b5e20;border-radius: 0.5em;color: white;font-weight: bold;padding: 2px 0.5em;'
 
+export default {
+  asyncData ({ app, route }) {
+    app.$log('groupCollapsed', `%cRoute: ${route.fullPath}`, defaultStyle)
+    app.$log('info', 'Message:', `this is ${process.server ? 'server' : 'client'} side log`)
+    app.$log('groupEnd')
+  },
+  mounted () {
+    this.$log('log', 'The page has mounted!')
+  }
 }
 </script>
