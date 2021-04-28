fbcfb26ecb8c89fd0c3f412a78f5c9ae8dcc19b8
Mohammad Saleh Fadaei <ms.fadaei1997@gmail.com>
2021-04-28 23:34:53 +0430

    test: add module test

--- test/module.test.js
+++ test/module.test.js
@@ -1,18 +1,62 @@
-const { setup, loadConfig, get } = require('@nuxtjs/module-test-utils')
+import { setupTest, expectModuleToBeCalledWith, getNuxt } from '@nuxt/test-utils'
+const { resolve } = require('path')
 
-describe('module', () => {
-  let nuxt
-
-  beforeAll(async () => {
-    ({ nuxt } = (await setup(loadConfig(__dirname, '../../example'))))
-  }, 60000)
-
-  afterAll(async () => {
-    await nuxt.close()
+describe('setup module without config', () => {
+  setupTest({
+    testDir: __dirname,
+    fixture: 'example',
+    rootDir: resolve(__dirname, '../example'),
+    config: {}
   })
 
-  test('render', async () => {
-    const html = await get('/')
-    expect(html).toContain('Works!')
+  const defaultConfig = {
+    namespace: 'console'
+  }
+
+  test('should inject plugin', () => {
+    // inject plugin
+    expectModuleToBeCalledWith('addPlugin', {
+      src: expect.stringContaining('store\\index.js'),
+      fileName: 'console\\store\\index.js',
+      options: defaultConfig
+    })
+
+    // inject store
+    expectModuleToBeCalledWith('addPlugin', {
+      src: expect.stringContaining('plugins\\index.js'),
+      fileName: 'console\\plugins\\index.js',
+      options: defaultConfig
+    })
+  })
+})
+
+describe('setup module with config', () => {
+  setupTest({
+    testDir: __dirname,
+    fixture: 'example',
+    rootDir: resolve(__dirname, '../example'),
+    config: {
+      browserConsole: {
+        namespace: 'logs'
+      }
+    }
+  })
+
+  test('should inject plugin', () => {
+    const { namespace } = getNuxt().options.browserConsole
+
+    // inject plugin
+    expectModuleToBeCalledWith('addPlugin', {
+      src: expect.stringContaining('store\\index.js'),
+      fileName: `${namespace}\\store\\index.js`,
+      options: getNuxt().options.browserConsole
+    })
+
+    // inject store
+    expectModuleToBeCalledWith('addPlugin', {
+      src: expect.stringContaining('plugins\\index.js'),
+      fileName: `${namespace}\\plugins\\index.js`,
+      options: getNuxt().options.browserConsole
+    })
   })
 })
