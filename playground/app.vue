52e0d4691c55102ee23ab8819d67e028552c940a
Mohammad Saleh Fadaei <ms.fadaei1997@gmail.com>
2022-02-06 13:37:54 +0330

    feat: init module

---
+++ playground/app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    Nuxt module playground!
+  </div>
+</template>
+
+<script setup>
+const { $console } = useNuxtApp()
+$console.log('Hello from Nuxt module playground form ' + (process.server ? 'server' : 'client'))
+</script>
